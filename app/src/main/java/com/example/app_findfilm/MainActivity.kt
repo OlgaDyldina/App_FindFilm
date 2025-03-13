fc9dcf8a56b9e7c8cb764f36c14fcd2f1ba1db17
Olga Dyldina <olga.dildina@yandex.ru>
2025-03-13 17:43:24 +0500

    Correct activity_main

--- app/src/main/java/com/example/app_findfilm/MainActivity.kt
+++ app/src/main/java/com/example/app_findfilm/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.app_findfilm
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
