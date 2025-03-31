97c514ff6f2fc727f017dbfd3edc96bdd399de3d
Olga Dyldina <olga.dildina@yandex.ru>
2025-03-31 09:54:53 +0500

    Complited task mudule_22

--- app/src/main/java/com/example/app_findfilm/MainActivity.kt
+++ app/src/main/java/com/example/app_findfilm/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.app_findfilm
 
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        TextView() = this.findViewById(R.layout.activity_main)
 
         initNavigation()
     }
